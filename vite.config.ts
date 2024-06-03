8b657931c4d9eb16bb74a0c50b4cb7e5f82f5342
SashaJarvi <alexandrermakov2@gmail.com>
2024-06-03 18:38:06 +0400

    added 'base' parameter for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-date-input/',
   plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
